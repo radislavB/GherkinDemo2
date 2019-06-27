42d08b6c8dceb584c3c2fc18862eeaec5656c8ff
elishevaHa <46865523+elishevaHa@users.noreply.github.com>
2019-06-27 11:31:28 +0300

    Update StepDefinitions.java

--- src/test/java/com/microfocus/adm/octane/gherkinautomation/StepDefinitions.java
+++ src/test/java/com/microfocus/adm/octane/gherkinautomation/StepDefinitions.java
@@ -20,7 +20,7 @@ public class StepDefinitions {
 
     @Given("a web browser is on the Google page")
     public void aWebBrowserIsOnTheGooglePage() throws Exception {
-        //throw new Exception("exception in given");
+        throw new Exception("exception in given");
     }
 
     @When("the search phrase {string} is entered")
