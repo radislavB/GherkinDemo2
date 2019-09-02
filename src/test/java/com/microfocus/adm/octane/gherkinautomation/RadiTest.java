248876cc952d6e6a5b66b46778b5a6a49ff8c9be
Radislav Berkovich <radislav.berkovich@microfocus.com>
2019-09-02 12:39:26 +0300

    adjustments for linux

--- src/test/java/com/microfocus/adm/octane/gherkinautomation/RadiTest.java
+++ src/test/java/com/microfocus/adm/octane/gherkinautomation/RadiTest.java
@@ -5,7 +5,7 @@ import cucumber.api.junit.Cucumber;
 import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(plugin={"com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results\\\\Radi_OctaneGherkinResults.xml"},
+@CucumberOptions(plugin={"com.hpe.alm.octane.OctaneGherkinFormatter:gherkin-results//Radi_OctaneGherkinResults.xml"},
         features="src/test/resources/dan")
 public class RadiTest {
 }
---
+++ src/test/resources/dan/Dan_1022.feature
@@ -0,0 +1,9 @@
+#Auto generated Octane revision tag
+@TID1021REV0
+Feature: feature name
+
+	Scenario: my s
+		Given g
+		When w
+		Then t
+		Then t
